464d797bbd4ee95d47b0cc1fbe83b14cfab3ba44
onigra <3280467rec@gmail.com>
2018-04-15 21:54:31 +0900

    add another options

--- src/KeycloakConfig.ts
+++ src/KeycloakConfig.ts
@@ -1,6 +1,11 @@
+type ResponseMode = "query" | "fragment";
+
 export default interface KeycloakConfig {
   realm: string;
   url: string;
   clientId: string;
   redirectUrl: string;
+  scope?: string;
+  responseMode?: ResponseMode;
+  responseType?: string;
 };
